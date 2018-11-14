51b9a0478f9d3709257699096d0f884d4aed4e16
Minki <alsrlminki@gmail.com>
2018-11-14 16:42:47 +0900

    commit

--- minki/minki/temp.cpp
+++ minki/minki/temp.cpp
@@ -1,17 +1,52 @@
+#include <stdio.h>
 
-#include <iostream> // cin 사용 위해 필요 
-#include <vector>	// vector 사용 위해 필요  
-#include <algorithm> // sort 사용 위해 필요 
-#include <string>
-#include <numeric>
-#include <sstream>
-
-
-using namespace std;
-
+int max(int a, int b)
+{
+	if (a < b)
+		return b;
+	else
+		return a;
+}
+int min(int a, int b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+}
 int main()
 {
-	
+	int tc;
+	int move, i, a[101], u, d, temp;
+	for (tc = 0; tc < 10; tc++)
+	{
+		scanf("%d", &move);
+		for (i = 1; i <= 100; i++)
+			a[i] = 0;
+		for (i = 0; i < 100; i++)
+		{
+			scanf("%d", &temp);
+			a[temp]++;
+		}
+		for (i = 99; i > 0; i--)
+			a[i] += a[i + 1];
+		u = 100, d = 1;
+		for (;;)
+		{
+			temp = min(min(a[u], 100 - a[d]), move);
+			a[u] -= temp;
+			a[d] += temp;
+			move -= temp;
+			if (a[u] == 0)
+				u--;
+			if (a[d] == 100)
+				d++;
+			if (d >= u || move == 0)
+				break;
+		}
 
+
+		printf("#%d %d\n", tc + 1, u - d + 1);
+	}
 	return 0;
 }
