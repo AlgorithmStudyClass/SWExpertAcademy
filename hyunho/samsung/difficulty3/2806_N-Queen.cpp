f198e67bc6354dc6188b1ec3b9995291f44de56c
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-12-08 15:27:07 +0900

    COMMIT // ~2806.cpp N-Queen

---
+++ hyunho/samsung/difficulty3/2806_N-Queen.cpp
@@ -0,0 +1,78 @@
+/*
+2806. N-Queen D3
+https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GKs06AU0DFAXB&categoryId=AV7GKs06AU0DFAXB&categoryType=CODE
+*/
+
+#include<iostream>
+#include<vector>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+using namespace std ;
+
+int cnt = 0 ;
+
+bool promising( int i , int *col ) {
+
+    int k = 1 ;
+    bool flag = true ;
+
+    while( k < i && flag ) {
+
+        if( col[i] == col[k] || abs( col[i] - col[k] ) == ( i - k ) )
+            flag = false ;
+
+        k++ ;
+    }
+    return flag ;
+}
+
+void queens( int i , int size , int *col ) {
+
+    for( int j = 1 ; j <= size ; j++ ) {
+
+        col[ i + 1 ] = j ;
+
+        if( promising( i , col ) ) {
+
+            if( i == size ) {
+
+                cnt++ ;
+                return ;
+
+            } else
+                queens( i + 1 , size , col ) ;
+        }
+    }
+}
+
+int main() {
+
+    int testSize = 0 ;
+    scanf("%d" , &testSize ) ;
+
+    int T = 0 ;
+    while( T++ < testSize ) {
+
+        int size = 0 ;
+        scanf("%d" , &size ) ;
+
+        int col[20] = { 0 } ;
+
+        queens( 0 , size , col ) ;
+
+        printf("#%d %d\n" , T , cnt ) ;
+        cnt = 0 ;
+
+    }
+    return 0 ;
+}
+
+
+
+
+
+
+
+
