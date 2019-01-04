51a088448f6b38fde4b8cd0571ea673b47fcc2bd
Minki <alsrlminki@gmail.com>
2019-01-04 21:11:04 +0900

    ~5688.Cubic root

---
+++ minki/minki/SW_Expert_Academy/D3/5688_Cubic root.cpp
@@ -0,0 +1,47 @@
+﻿/*
+	5688. 세제곱근을 찾아라 - D3
+	https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXVyCaKugQDFAUo&categoryId=AWXVyCaKugQDFAUo&categoryType=CODE
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int t, line = 1;
+	long long int triple, n ,answer;
+	scanf("%d", &t);
+	while (0 < t--) {
+		scanf("%lld", &n);
+		answer = 1;
+		//sol1
+		/*while (1) {
+			triple = answer * answer * answer;
+			if (triple == n)
+				break;
+			else if (triple > n) {
+				answer = -1;
+				break;
+			}
+			answer++;
+		}
+		*/
+		//sol2
+		long long int l = 1, r = 1e6, mid, dab = -1;//1e6 = 10^6
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+            if (mid * mid * mid == n) { dab = mid; break; }
+            else if (mid * mid * mid > n) r = mid - 1;
+            else l = mid + 1;
+        }
+		
+		printf("#%d %d\n", line++, dab);
+	}
+
+	return 0;
+}
