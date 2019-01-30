022b57b97b3d6c52823681f3220db776380be1a5
Unknown <gusgh1492@mail.hongik.ac.kr>
2019-01-30 19:36:54 +0900

    COMMIT // 2try // graph // DAG( directed acyclic graph )

--- hyunho/samsung/SDS_Algorithm_Lecture/#3_Graph/2252_Line up.cpp
+++ hyunho/samsung/SDS_Algorithm_Lecture/#3_Graph/2252_Line up.cpp
@@ -2,56 +2,75 @@
 2252. 줄 세우기
 https://www.acmicpc.net/problem/2252
 
-위상정렬
+위상정렬 DAG
 인접 리스트로 표현
 */
 
 #include<iostream>
+#include<vector>
 #include<queue>
-#include <vector>
 using namespace std ;
 
 int N , M ;
-vector< int > arr[ 32001 ] ;        //  vector< pair<int , int > > 로 안하는 이유는 indegree 만 필요해서
-int degree[ 32001 ] ;
+vector<int> list[32001] ;
+int indegree[32001] ;
 queue<int> que ;
-int a , b ;
 
 int main() {
 
-    scanf( "%d %d" , &N , &M ) ;
+    scanf("%d %d" , &N , &M ) ;
 
-    for( int i = 0 ; i < M ; i++ ) {
+    for( int i = 0 , a , b ; i < M ; i++ ) {
 
-        scanf( "%d %d" , &a , &b ) ;
-
-        arr[a].push_back(b);     //  인접 리스트 생성
-        degree[b]++;            //  in degree ++ 
+        scanf("%d %d" , &a , &b ) ;
+        list[a].push_back( b ) ;
+        indegree[b]++ ;
     }
 
     for( int i = 1 ; i <= N ; i++ ) {
-
-        if( degree[i] == 0 )
+        if( indegree[i] == 0 )
             que.push( i ) ;
     }
 
-    int result = 0 ;
-
     while( !que.empty() ) {
 
-        result = que.front() ;
-        printf( "%d " , result ) ; que.pop() ;
+        int tmp = que.front() ; que.pop() ;
+        printf("%d " , tmp ) ;
 
-        for( int i = 0 ; i < arr[result].size() ; i++ ) {
+        for( int i = 0 ; i < list[tmp].size() ; i++ ) {
 
-            int p = arr[result][i] ;
-            degree[p]-- ;
+            int to = list[tmp][i] ;
+            indegree[to]-- ;
 
-            if( degree[p] == 0 )
-                que.push(p) ;
+            if( indegree[to] == 0 )
+                que.push(to) ;
         }
     }
-
+    printf("\n") ;
     return 0 ;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
