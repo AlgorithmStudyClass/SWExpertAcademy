f4ca4d57548e2ff2cac401b6e7b8427cabe7fbe9
Unknown <gusgh1492@mail.hongik.ac.kr>
2019-02-12 20:08:12 +0900

    COMMIT // 2try // DP // 11049.cpp

--- hyunho/samsung/SDS_Algorithm_Lecture/#6_Dynamic programming/11049_Matrix multiplication order.cpp
+++ hyunho/samsung/SDS_Algorithm_Lecture/#6_Dynamic programming/11049_Matrix multiplication order.cpp
@@ -3,13 +3,40 @@
 https://www.acmicpc.net/problem/11049
 
 dp
+D[i][j] : i행에서 j행까지 곱했을 때 최소 곱셈연산 수
 */
 
 
+#include<iostream>
+using namespace std ;
 
+int N ;
+int list[501][3] ;
+int D[501][501] ;
 
+int main() {
 
+	scanf("%d" , &N ) ;
 
+	for( int i = 1 ; i <= N ; i++ )
+		scanf("%d %d" , &list[i][1] , &list[i][2] ) ;
+
+	for( int i = 1 ; i < N ; i++ )
+		D[i][i+1] = list[i][1] * list[i][2] * list[i+1][2] ;
+
+	for ( int l=2; l<=N; l++ ) {
+        for ( int i=1; i<=N-l; i++ ) {
+            int j = i + l;
+            for ( int k=i; k<j; k++ ) {
+                int c = D[i][k] + D[k+1][j] + (list[i][1] * list[k][2] * list[j][2]);
+                if ( D[i][j] == 0 || D[i][j] > c ) D[i][j] = c;
+            }
+        }
+    }
+
+    printf("%d\n" , D[1][N] ) ;
+    return 0 ;
+}
 
 
 
